072852d406023bb4e33a1726a4c8d55c4c912675
raomengyang <raomengyang@163.com>
2016-01-18 22:33:47 +0800

    new project about JianWeather

---
+++ JianWeather/app/src/main/java/com/dreamyrao/jianweather/utils/Constant.java
@@ -0,0 +1,9 @@
+package com.dreamyrao.jianweather.utils;
+
+/**
+ * Created by raomengyang on 1/18/16.
+ */
+public class Constant {
+
+
+}
---
+++ JianWeather/app/src/main/java/com/dreamyrao/jianweather/utils/HttpUtils.java
@@ -0,0 +1,7 @@
+package com.dreamyrao.jianweather.utils;
+
+/**
+ * Created by raomengyang on 1/18/16.
+ */
+public class HttpUtils {
+}
