95ca678512175086437f8b2ddbdfff4ad70a2d63
raomengyang <raomengyang@163.com>
2016-01-22 01:04:52 +0800

    更新bean和更多的数据

--- JianWeather/app/src/main/java/com/dreamyrao/jianweather/bean/WeatherBean.java
+++ JianWeather/app/src/main/java/com/dreamyrao/jianweather/bean/WeatherBean.java
@@ -19,6 +19,34 @@ public class WeatherBean {
     private String lat;
     private String lon;
 
+    private String aqi;
+    private String co;
+    private String no2;
+    private String o3;
+    private String pm10;
+    private String pm25;
+    private String qlty;
+    private String so2;
+
+    private String level;
+    private String stat;
+    private String title;
+    private String txt;
+    private String type;
+
+    private String code;
+
+    private String fl;
+    private String hum;
+    private String pcpn;
+    private String pres;
+    private String tmp;
+    private String vis;
+    private String deg;
+    private String dir;
+    private String sr;
+    private String spd;
+
     public String getWeatherServiceName() {
         return weatherServiceName;
     }
@@ -75,6 +103,198 @@ public class WeatherBean {
         this.lon = lon;
     }
 
+    public String getAqi() {
+        return aqi;
+    }
+
+    public void setAqi(String aqi) {
+        this.aqi = aqi;
+    }
+
+    public String getCo() {
+        return co;
+    }
+
+    public void setCo(String co) {
+        this.co = co;
+    }
+
+    public String getNo2() {
+        return no2;
+    }
+
+    public void setNo2(String no2) {
+        this.no2 = no2;
+    }
+
+    public String getO3() {
+        return o3;
+    }
+
+    public void setO3(String o3) {
+        this.o3 = o3;
+    }
+
+    public String getPm10() {
+        return pm10;
+    }
+
+    public void setPm10(String pm10) {
+        this.pm10 = pm10;
+    }
+
+    public String getPm25() {
+        return pm25;
+    }
+
+    public void setPm25(String pm25) {
+        this.pm25 = pm25;
+    }
+
+    public String getQlty() {
+        return qlty;
+    }
+
+    public void setQlty(String qlty) {
+        this.qlty = qlty;
+    }
+
+    public String getSo2() {
+        return so2;
+    }
+
+    public void setSo2(String so2) {
+        this.so2 = so2;
+    }
+
+    public String getLevel() {
+        return level;
+    }
+
+    public void setLevel(String level) {
+        this.level = level;
+    }
+
+    public String getStat() {
+        return stat;
+    }
+
+    public void setStat(String stat) {
+        this.stat = stat;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getTxt() {
+        return txt;
+    }
+
+    public void setTxt(String txt) {
+        this.txt = txt;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getFl() {
+        return fl;
+    }
+
+    public void setFl(String fl) {
+        this.fl = fl;
+    }
+
+    public String getHum() {
+        return hum;
+    }
+
+    public void setHum(String hum) {
+        this.hum = hum;
+    }
+
+    public String getPcpn() {
+        return pcpn;
+    }
+
+    public void setPcpn(String pcpn) {
+        this.pcpn = pcpn;
+    }
+
+    public String getPres() {
+        return pres;
+    }
+
+    public void setPres(String pres) {
+        this.pres = pres;
+    }
+
+    public String getTmp() {
+        return tmp;
+    }
+
+    public void setTmp(String tmp) {
+        this.tmp = tmp;
+    }
+
+    public String getVis() {
+        return vis;
+    }
+
+    public void setVis(String vis) {
+        this.vis = vis;
+    }
+
+    public String getDeg() {
+        return deg;
+    }
+
+    public void setDeg(String deg) {
+        this.deg = deg;
+    }
+
+    public String getDir() {
+        return dir;
+    }
+
+    public void setDir(String dir) {
+        this.dir = dir;
+    }
+
+    public String getSr() {
+        return sr;
+    }
+
+    public void setSr(String sr) {
+        this.sr = sr;
+    }
+
+    public String getSpd() {
+        return spd;
+    }
+
+    public void setSpd(String spd) {
+        this.spd = spd;
+    }
+
     public static WeatherBean createFromJsonString(String weatherInfo) {
         if (weatherInfo != null && weatherInfo.length() > 0) {
             WeatherBean weatherBean = new WeatherBean();
@@ -83,15 +303,22 @@ public class WeatherBean {
                 if (jsonObject.has("HeWeather data service 3.0")) {
                     JSONArray jsonArray = new JSONArray(jsonObject.getString("HeWeather data service 3.0"));
                     JSONObject jsonObject1 = jsonArray.getJSONObject(0);
-//                    Log.e(TAG, jsonObject1.getString("basic").toString());
-                    JSONObject jsonObject2 = jsonObject1.getJSONObject("basic");
-                    weatherBean.setCity(jsonObject2.getString("city"));
-                    weatherBean.setCountry(jsonObject2.getString("cnty"));
-//                    for (int i = 0; i < jsonArray1.length(); i++) {
-//                        JSONObject jsonObject2 = new JSONArray().getJSONObject(i);
-//                        Log.e(TAG, jsonObject2.getString("city"));
-//                        weatherBean.setCity(jsonObject2.getString("city"));
-//                    }
+                    JSONObject jsonObject5 = jsonObject1.getJSONObject("basic");
+                    JSONObject jsonObject2 = jsonObject1.getJSONObject("aqi");
+                    JSONObject jsonObject3 = jsonObject2.getJSONObject("city");
+                    JSONObject jsonObject4 = jsonObject1.getJSONObject("now");
+
+                    weatherBean.setCity(jsonObject5.getString("city"));
+                    weatherBean.setCountry(jsonObject5.getString("cnty"));
+
+                    weatherBean.setAqi(jsonObject3.getString("aqi"));
+                    weatherBean.setPm25(jsonObject3.getString("pm25"));
+                    weatherBean.setQlty(jsonObject3.getString("qlty"));
+
+                    weatherBean.setFl(jsonObject4.getString("fl"));
+                    weatherBean.setHum(jsonObject4.getString("hum"));
+                    weatherBean.setTmp(jsonObject4.getString("tmp"));
+
                 }
             } catch (JSONException e) {
                 e.printStackTrace();
--- JianWeather/app/src/main/java/com/dreamyrao/jianweather/ui/activity/MainActivity.java
+++ JianWeather/app/src/main/java/com/dreamyrao/jianweather/ui/activity/MainActivity.java
@@ -1,6 +1,7 @@
 package com.dreamyrao.jianweather.ui.activity;
 
 import android.os.Bundle;
+import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
@@ -8,7 +9,6 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.android.volley.RequestQueue;
 import com.android.volley.VolleyError;
@@ -21,8 +21,14 @@ import com.dreamyrao.jianweather.utils.Executable;
 import com.dreamyrao.jianweather.utils.HttpUtils;
 
 public class MainActivity extends AppCompatActivity {
-    private TextView weatherTV;
+    private TextView cityNameTV;
+    private TextView tmpValueTV;
+    private TextView humValueTV;
+    private TextView flValueTV;
+    private TextView pm25TV;
+    private TextView airQltyTV;
     private Toolbar toolbar;
+    private CollapsingToolbarLayout toolbarLayout;
     private FloatingActionButton fab;
 
     private RequestQueue queue;
@@ -39,11 +45,20 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void initView() {
-        weatherTV = (TextView) findViewById(R.id.tv_main_content);
+        cityNameTV = (TextView) findViewById(R.id.tv_city_name);
+        tmpValueTV = (TextView) findViewById(R.id.tv_value_tmp);
+        humValueTV = (TextView) findViewById(R.id.tv_value_hum);
+        airQltyTV = (TextView) findViewById(R.id.tv_value_quality);
+        flValueTV = (TextView) findViewById(R.id.tv_value_fl);
+        pm25TV = (TextView) findViewById(R.id.tv_value_pm25);
+
+        toolbarLayout = (CollapsingToolbarLayout) findViewById(R.id.toolbar_layout);
         toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
         fab = (FloatingActionButton) findViewById(R.id.fab);
+        getWeatherInfo();
+
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -69,7 +84,13 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void execute(String s) {
 //                Toast.makeText(MainActivity.this, s.toString(), Toast.LENGTH_SHORT).show();
-                weatherTV.setText(WeatherBean.createFromJsonString(s.toString()).getCity());
+                cityNameTV.setText(weatherBean.createFromJsonString(s.toString()).getCity());
+                tmpValueTV.setText(weatherBean.createFromJsonString(s.toString()).getTmp());
+                toolbarLayout.setTitle("当前温度：" + tmpValueTV.getText().toString());
+                humValueTV.setText(weatherBean.createFromJsonString(s.toString()).getHum());
+                airQltyTV.setText(weatherBean.createFromJsonString(s.toString()).getQlty());
+                flValueTV.setText(weatherBean.createFromJsonString(s.toString()).getFl());
+                pm25TV.setText(weatherBean.createFromJsonString(s.toString()).getPm25());
             }
         }, new Executable<VolleyError>() {
             @Override
